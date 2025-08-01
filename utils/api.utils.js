73578a4d3540be6553fd80ab91543c4f459847d8
Do Mac <rittikiert.san@gmail.com>
2025-08-01 23:18:54 +0700

    update

--- utils/api.utils.js
+++ utils/api.utils.js
@@ -1,3 +1,22 @@
+// import axios from "axios";
+
+// const axiosInstance = axios.create({
+//   baseURL: process.env.NEXT_PUBLIC_API_URL,
+// headers: {
+//   "Content-Type": "application/json",
+// },
+// });
+
+// axiosInstance.interceptors.request.use((config) => {
+//   const token = localStorage.getItem("token");
+//   if (token) {
+//     config.headers.Authorization = `Bearer ${token}`;
+//   }
+//   return config;
+// });
+
+// export default axiosInstance;
+
 import axios from "axios";
 
 const axiosInstance = axios.create({
@@ -8,10 +27,14 @@ const axiosInstance = axios.create({
 });
 
 axiosInstance.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
+  if (typeof window !== "undefined") {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
   }
+
+  config.headers["Content-Type"] = "application/json";
   return config;
 });
 
