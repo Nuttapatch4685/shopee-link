be9ba566b27db0d31240c6a3bd129c9c5dee017b
Do Mac <rittikiert.san@gmail.com>
2025-08-01 23:29:29 +0700

    update

--- pages/api/users/create.js
+++ pages/api/users/create.js
@@ -2,9 +2,15 @@ import axios from "@/utils/api.utils";
 
 export default async function handler(req, res) {
   let payload = JSON.parse(req.body);
+  const token = localStorage.getItem("token");
 
   await axios
-    .post(`${process.env.NEXT_PUBLIC_API_URL}/api/users`, payload)
+    .post(`${process.env.NEXT_PUBLIC_API_URL}/api/users`, payload, {
+      headers: {
+        Authorization: token, // Forward the token
+        "Content-Type": "application/json",
+      },
+    })
     .then((data) => {
       if (data.status === 200) {
         res.status(200).json(data.data);
--- pages/api/users/update.js
+++ pages/api/users/update.js
@@ -2,9 +2,15 @@ import axios from "@/utils/api.utils";
 
 export default async function handler(req, res) {
   let payload = JSON.parse(req.body);
+  const token = localStorage.getItem("token");
 
   await axios
-    .put(`${process.env.NEXT_PUBLIC_API_URL}/api/users/${payload.user_id}`, payload)
+    .put(`${process.env.NEXT_PUBLIC_API_URL}/api/users/${payload.user_id}`, payload, {
+      headers: {
+        Authorization: token, // Forward the token
+        "Content-Type": "application/json",
+      },
+    })
     .then((data) => {
       console.log("response update", data);
       if (data.status === 200) {
