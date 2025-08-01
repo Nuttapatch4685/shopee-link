f1fffb5c06596bcf6f799387f0eaced583fc2364
Do Mac <rittikiert.san@gmail.com>
2025-08-01 22:53:25 +0700

    update

--- pages/api/users/update.js
+++ pages/api/users/update.js
@@ -12,8 +12,9 @@ export default async function handler(req, res) {
       }
     })
     .catch((e) => {
-      console.log("Update user frontend", e.message);
+      console.log("Update user frontend : ", e.message);
       let response = e.response;
+      console.log("response catch : ", response);
       res.status(400).json(response.data);
     });
 }
