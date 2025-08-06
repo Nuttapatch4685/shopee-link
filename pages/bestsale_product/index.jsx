bb70cfd1da8311ab618890011443ae902e92dd9c
Nattapach Tiamted <nuttapatch4685@gmail.com>
2025-08-07 06:26:24 +0700

    update

--- pages/bestsale_product/index.jsx
+++ pages/bestsale_product/index.jsx
@@ -63,7 +63,7 @@ const BestSaleProducts = () => {
 
         <div className="flex flex-wrap gap-3 mb-4">
           <button
-            onClick={extractCommissionProducts}
+            onClick={extractCommissionLinks}
             className="bg-orange-600 text-white px-4 py-2 rounded"
           >
             ดึงสินค้ามีค่าคอม
